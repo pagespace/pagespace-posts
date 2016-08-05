c254e984229caec4d49c2d7cdccd594a13c39d98
Phil Mander <philip.mander@gmail.com>
2016-08-05 11:42:23 +0200

    Fix sort by date issues

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var Promise = require('bluebird');
 var mongoose = require('mongoose');
+mongoose.Promise = Promise;
 
 module.exports = {
     process: function(config, opts) {
@@ -15,7 +16,6 @@ module.exports = {
         }
 
         //configure query
-        var sortField = config.sort || '-publishedAt';
         var pageQuery = {};
         if(parent) {
             pageQuery.parent = new mongoose.Types.ObjectId(parent);
@@ -33,40 +33,41 @@ module.exports = {
         //query pages
         var preview = opts.preview;
         var PageModel = pagespace.getPageModel(preview);
-        var query = PageModel.find(pageQuery).populate('regions.includes.plugin regions.includes.include').sort(sortField);
-        var findPagePromise = Promise.promisify(query.exec, query);
-        return findPagePromise().map(function(page) {
+        var findPagePromise = PageModel.find(pageQuery).populate('regions.includes.plugin regions.includes.include').exec();
+        return findPagePromise.map(function(page) {
             var post = {};
             post.title = page.name;
             post.url = page.url;
             post.date = page.publishedAt || page.createdAt;
             var postIncludes = [];
-            for(var i = 0; i < page.regions.length; i++) {
-                var region = page.regions[i];
-                if(region.name === regionName) {
-                    for(var j = 0; j < region.includes.length; j++) {
-                        var include = region.includes[j];
-                        if(include.plugin) {
-                            var pluginModule = pagespace.pluginResolver.require(include.plugin ? include.plugin.module : null);
-                            if (pluginModule) {
-                                var includeData = include.include && include.include.data ? include.include.data : {};
-                                if (typeof pluginModule.process === 'function') {
-                                    postIncludes.push(pluginModule.process(includeData, {
-                                        preview: opts.preview,
-                                        reqUrl: opts.reqUrl,
-                                        reqMethod: opts.reqMethod
-                                    }));
-                                } else {
-                                    postIncludes.push(includeData);
-                                }
-                            }
+            const region = page.regions.find(region => region.name === regionName);
+
+            for(let include of region.includes) {
+                if(include.plugin) {
+                    var pluginModule = pagespace.pluginResolver.require(include.plugin ? include.plugin.module : null);
+                    if (pluginModule) {
+                        var includeData = include.include && include.include.data ? include.include.data : {};
+                        if (typeof pluginModule.process === 'function') {
+                            postIncludes.push(pluginModule.process(includeData, {
+                                preview: opts.preview,
+                                reqUrl: opts.reqUrl,
+                                reqMethod: opts.reqMethod
+                            }));
+                        } else {
+                            postIncludes.push(includeData);
                         }
                     }
                 }
             }
+
             post.includes = postIncludes;
             return Promise.props(post);
         }).then(function(posts) {
+            posts.sort(function(a, b) {
+                a = new Date(a.date);
+                b = new Date(b.date);
+                return a > b ? -1 : a < b ? 1 : 0;
+            });
             return {
                 wrapperClass: config.wrapperClass,
                 posts: posts
